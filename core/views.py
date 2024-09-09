e3749eccebe200218319f171afe692d411db8e27
NikPeg <peganov.nik@gmail.com>
2024-09-09 10:34:20 +0300

    Run task async

--- core/views.py
+++ core/views.py
@@ -11,7 +11,7 @@ def home(request):
     return render(request, 'home.html')
 
 
-def listen(request):
+async def listen(request):
     # Process the parameters and start the main function asynchronously
     session_slug = request.GET.get('session')
     trading_pair = request.GET.get('pair')
@@ -20,7 +20,16 @@ def listen(request):
     if not session_slug or not trading_pair or not streams_count:
         return JsonResponse({'error': 'Missing required parameters'}, status=400)
 
-    asyncio.run(listen_streams(session_slug, streams_count, trading_pair.replace('/', '').lower()))
+    # Ensure streams_count is an integer
+    try:
+        streams_count = int(streams_count)
+    except ValueError:
+        return JsonResponse({'error': 'Invalid streams parameter'}, status=400)
+
+    # Kick off the listen_streams asynchronously
+    asyncio.create_task(
+        listen_streams(session_slug, streams_count, trading_pair.replace('/', '').lower())
+    )
     return render(request, 'listen.html')
 
 
